8e9d8d64abbe5c3d3a9a392d01aca4d5f2f8fe50
yuki-yamada-man <major.in.from@i.softbank.jp>
2021-11-03 18:17:06 +0000

    first

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  
+  def index
+  end
+  
+  def show 
+  end
+  
+  def edit 
+  end 
+  
+  def update 
+  end
+  
+  
+  private
+  
+  def user_params 
+    params.require(:user).permit(:name,:introduction,:profile_image)
+  end
+end
