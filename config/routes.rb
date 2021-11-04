c74759929532ec8ac5d328a62f36e7eb5a8a66c7
yuki-yamada-man <major.in.from@i.softbank.jp>
2021-11-04 06:44:21 +0000

    second

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+
+  resources :users, only: [:index,:show,:edit,:update]
+  resources :books
+
+
 end
