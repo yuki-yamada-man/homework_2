5a6e5078c2475a201784141d19222ae6b7fd64ed
yuki-yamada-man <major.in.from@i.softbank.jp>
2021-11-06 14:29:28 +0000

    コントローラーの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,50 @@
 class BooksController < ApplicationController
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book_new = Book.new
+    @book_new.user.id = current_user.id
+    if @book.save(book_params)
+      redirect_to book_path(@book.id)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
+    @book_new = Book.all
+    @user = current_user
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,37 @@
 class UsersController < ApplicationController
-  
+
   def index
+    @book_new = Book.new
+    @user = current_user
+    @users = User.all
   end
-  
-  def show 
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.book
+    @book_new = Book.new
   end
-  
-  def edit 
-  end 
-  
-  def update 
+
+  def edit
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
-  
-  
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.path)
+    else
+      render :edit
+    end
+  end
+
+
   private
-  
-  def user_params 
+
+  def user_params
     params.require(:user).permit(:name,:introduction,:profile_image)
   end
 end
